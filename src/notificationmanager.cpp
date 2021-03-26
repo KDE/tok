b8c12e48fedd82de5ace415150604683b47e26b8
Jan Blackquill <uhhadd@gmail.com>
2021-03-25 22:25:38 -0400

    more work on notifications

--- src/notificationmanager.cpp
+++ src/notificationmanager.cpp
@@ -12,23 +12,39 @@ NotificationManager::~NotificationManager()
 
 }
 
+#define match(n) { auto& _matchIt = n; switch(n->get_id()) {
+#define endmatch } }
+
+#define handle(c, v) case c::ID: { auto v = static_cast<c*>(_matchIt.get());
+#define endhandle }
+
 void NotificationManager::handleUpdateActiveNotifications(TDApi::object_ptr<TDApi::updateActiveNotifications> activeNotifications)
 {
-    qDebug() << "do sound";
-
-    auto notif = new KNotification("newMessage");
-    notif->setText("eww");
-    notif->sendEvent();
 }
 
 void NotificationManager::handleUpdateNotificationGroup(TDApi::object_ptr<TDApi::updateNotificationGroup> notificationGroup)
 {
-    qDebug() << "update notification group";
+    for (auto& n : notificationGroup->added_notifications_) {
+        using namespace TDApi;
+
+        match (n->type_)
+            handle (notificationTypeNewMessage, msg)
+                match (msg->message_->content_)
+                    handle (messageText, tmsg)
+                        auto notif = new KNotification("newMessage");
+                        notif->setText(QString::fromStdString(tmsg->text_->text_));
+                        notif->setHint("resident", true);
+                        notif->sendEvent();
+                    endhandle
+                endmatch
+            endhandle
+        endmatch
+
+    }
 }
 
 void NotificationManager::handleUpdateNotification(TDApi::object_ptr<TDApi::updateNotification> notification)
 {
-    qDebug() << "update notification";
 }
 
 void NotificationManager::handleUpdateHavePendingNotifications(TDApi::object_ptr<TDApi::updateHavePendingNotifications> havePendingNotification)
