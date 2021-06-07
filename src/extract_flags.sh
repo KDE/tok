79fd3d611c395b58cf5c8a207742b61869ffe41d
Jan Blackquill <uhhadd@gmail.com>
2021-06-07 17:18:04 -0400

    build, fix: reintroduce extract_flags.sh
    This got nuked somehow. Oops.

---
+++ src/extract_flags.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+bail() {
+	echo $@
+	exit 1
+}
+
+has() {
+  type -p "$1" >/dev/null
+}
+
+has cmake || bail "CMake is missing; please install CMake"
+has jq || bail "jq is missing; please install jq"
+
+dir=$(mktemp -d)
+
+cat > "$dir/CMakeLists.txt" << EndOfMessage
+set(REQUIRED_QT_VERSION 5.15.0)
+
+project(Tok LANGUAGES CXX)
+
+cmake_minimum_required(VERSION 3.5)
+
+$1
+
+add_executable(tok null.cpp)
+
+target_link_libraries(tok
+  PUBLIC
+    $2
+)
+
+EndOfMessage
+
+touch "$dir/null.cpp"
+
+cd "$dir"
+mkdir _build
+cd _build
+mkdir -p ".cmake/api/v1/query"
+touch ".cmake/api/v1/query/codemodel-v2"
+cmake .. 2>&1 >nohup.out
+retval=$?
+
+if [ $retval -ne 0 ]; then
+    cat nohup.out
+    exit $retval
+fi
+
+jq -r ".link.commandFragments[].fragment" .cmake/api/v1/reply/target-*.json || bail "Check out the logs in" $PWD
+
+echo "===="
+
+jq -r ".compileGroups[].includes[].path" .cmake/api/v1/reply/target-*.json || bail "Check out the logs in" $PWD
