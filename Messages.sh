c12a61d883fbcb5d9957c22b67282945497b6f0e
Jan Blackquill <uhhadd@gmail.com>
2021-08-12 17:04:04 -0400

    tooling: update Messages.sh to not die when ran against Tok's repo

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h -o -name \*.qml` -o $podir/tok.pot
+python - << EOF
+
+import subprocess
+import os
+import re
+
+sfiles = []
+
+for subdir, dirs, files in os.walk('.'):
+	sfiles += [subdir+'/'+f for f in files if re.match(".*\.(cpp|qml|cc|h|js)$", f)]
+
+xgettext = (os.getenv("XGETTEXT") or "xgettext").split()
+outdir = (os.getenv("podir") or "po") + "/tok.pot"
+
+subprocess.check_output(xgettext + sfiles + ['-o', outdir])
+
+EOF
